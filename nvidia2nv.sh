752c465d6ae7fd1f70bc12f811e178449564c9d2
bout10bucks <bout10bucks@gmail.com>
2013-11-04 00:35:25 -0600

    updated tmux scripts

--- nvidia2nv.sh
+++ nvidia2nv.sh
@@ -12,7 +12,7 @@
  #sed -i 's//nouveau/' /etc/modules-load.d/whitelist.conf
  echo nouveau >> /etc/modules-load.d/whitelist.conf
 
- pacman -Rdds --noconfirm nvidia-304xx{,-utils} lib32-nvidia-304xx-utils
+ pacman -Rdds --noconfirm nvidia{,-utils} lib32-nvidia-utils
  pacman -S --noconfirm nouveau-dri xf86-video-nouveau lib32-nouveau-dri
  
  #mkinitcpio -p linux
